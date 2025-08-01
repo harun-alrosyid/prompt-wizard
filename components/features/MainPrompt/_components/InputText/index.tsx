4b1c8cb2f071e9a50d3450838934e42b35716519
harun-alrosyid <alrosyid07@gmail.com>
2025-08-01 10:32:23 +0700

    feat: add model dummy and create text area component

--- components/features/MainPrompt/_components/InputText/index.tsx
+++ components/features/MainPrompt/_components/InputText/index.tsx
@@ -1,19 +1,23 @@
 import { FunctionComponent } from 'react';
 
-import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { TextArea } from '@/components/ui/textarea';
 
-interface InputTextProps extends React.HTMLAttributes<HTMLInputElement> {
-    label: string
-    subLabel?: string
+interface InputTextProps extends React.HTMLAttributes<HTMLTextAreaElement> {
+  label: string;
+  subLabel?: string;
 }
 
-const InputText: FunctionComponent<InputTextProps> = ({label,subLabel,...attrs}) => {
+const InputText: FunctionComponent<InputTextProps> = ({
+  label,
+  subLabel,
+  ...attrs
+}) => {
   return (
     <div className="grid gap-1">
       <Label>{label}</Label>
       <Label className="text-xs">{subLabel}</Label>
-      <Input {...attrs}/>
+      <TextArea {...attrs} />
     </div>
   );
 };
