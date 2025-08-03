622049173041e1e8d1326b1ea63daf4124d98fb8
harun-alrosyid <alrosyid07@gmail.com>
2025-08-03 22:56:05 +0700

    feat: add markdown component

--- components/features/MainPrompt/_components/PromptResult/index.tsx
+++ components/features/MainPrompt/_components/PromptResult/index.tsx
@@ -1,4 +1,6 @@
 import { FunctionComponent } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 import { Card } from '@/components/ui/card';
 
@@ -6,9 +8,11 @@ import { Card } from '@/components/ui/card';
 interface PromptResultProps extends React.HTMLAttributes<HTMLDivElement> {}
 const PromptResult: FunctionComponent<PromptResultProps> = ({ children }) => {
   return (
-    <Card className="w-2/3 h-[100%] rounded-s-sm rounded-e-sm p-8 relative">
+    <Card className="w-2/3 h-[100%] overflow-auto rounded-s-sm rounded-e-sm p-8 relative">
       <Card className="w-[100%] bg-gray-100 h-[100%] rounded-s-sm rounded-e-sm relative">
-        {children}
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+          {children as string}
+        </ReactMarkdown>
       </Card>
     </Card>
   );
